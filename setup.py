cd0a4e3dd9084b3cf2c22242befa376a981183cc
johanespelund <johan.espelund@hotmail.com>
2024-09-09 09:36:34 +0200

    Fix packages in setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def readfile(filename):
     with open(filename, "r") as f:
         return f.read()
 
+
 def parse_requirements(filename):
     """Load requirements from a pip requirements file."""
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         return f.read().splitlines()
+
 
 setup(
     name="FoamUtils",
     version="0.1",
     description="Utility functions for OpenFOAM",
-    long_description=readfile('README.md'),
+    long_description=readfile("README.md"),
     author="Johan R. Espelund",
     author_email="johan.espelund@sintef.no",
     url="https://github.com/johanespelund/FoamUtils",
-    py_modules=[
-        "plot_residuals",
-        "ThermophysicalProperties",
-        "file_utils",
-        "wall_profiles",
-    ],
-    install_requires=parse_requirements('requirements.txt'),
+    packages=find_packages(),
+    install_requires=parse_requirements("requirements.txt"),
     entry_points={
         "console_scripts": [
             "plot_res = FoamUtils.plot_residuals:main",
