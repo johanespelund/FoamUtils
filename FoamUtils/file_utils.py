316329cb3c09cf28fcfe27416b380935269ac2c1
Johan R. Espelund <johan.espelund@sintef.no>
2024-09-15 11:15:43 +0200

    Add load_wallHeatFlux

--- FoamUtils/file_utils.py
+++ FoamUtils/file_utils.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 
 def get_sorted_times(dir):
@@ -28,4 +29,37 @@ def read_parameters(path="parameters"):
                 value = value.strip()[:-1]
             p[key] = value
     return p
+
+def load_wallHeatFlux(filename, start=0, end=-1):
+    wall_heat_flows = {}
+    times = set()
+
+    # Find unique patch names
+    lines = open(filename, "r").readlines()
+    for line in lines:
+        if line[0] != "#":
+            line = line.rstrip().split("\t")
+            patch_name = line[1]
+            if patch_name not in wall_heat_flows:
+                wall_heat_flows[patch_name] = []
+            else:
+                break
+
+    # Read wall heat flows
+    lines = open(filename, "r").readlines()
+    for line in lines[
+        : -len(wall_heat_flows)
+    ]:  # Last time is for some reason written twice, so don't read last entries
+        if line[0] != "#":
+            line = line.rstrip().split("\t")
+            time = float(line[0])
+            patch_name = line[1]
+            times.add(time)
+            wall_heat_flows[patch_name].append(float(line[-1]))
+
+    times = np.array(sorted(list(times)))[start:end]
+    for key in wall_heat_flows:
+        wall_heat_flows[key] = np.array(wall_heat_flows[key])[start:end]
+
+    return times, wall_heat_flows
 
