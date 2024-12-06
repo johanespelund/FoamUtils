f0145293a7a5f0b9971ff7ef47dd79f8b7cb98dd
johanespelund <johan.espelund@hotmail.com>
2024-12-06 11:37:45 +0100

    v0.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def parse_requirements(filename):
 
 setup(
     name="FoamUtils",
-    version="0.2",
+    version="0.3",
     description="Utility functions for OpenFOAM",
     long_description=readfile("README.md"),
     author="Johan R. Espelund",
